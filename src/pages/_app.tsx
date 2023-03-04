186e02bb446164c573ba03916d87c4ae5a3f2d51
Jason Zhao <jason1382311@gmail.com>
2023-03-03 22:22:11 -0800

    set up trpc backend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
 import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
+import "../styles/global.css";
+const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
+export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { trpc } from "@/utils/trpc";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="text-2xl text-red-200">COW</div>;
+  const { data, isLoading } = trpc.hello.useQuery({ text: "client" });
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center mb-2">Which Pokemon is rounder?</div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl item-center">
+        <div className="w-16 h-16 bg-red-200">meow</div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 }
