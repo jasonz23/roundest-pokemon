acc1d8a508f613562655d1cba7072e24b70a1de0
Jason Zhao <jason1382311@gmail.com>
2023-03-05 15:17:46 -0800

    show wrong notification, answer will be corret if stats are equal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import {
   getRandomBaseStatIndex,
   getRandomPokemon,
 } from "@/utils/pokemon";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
 
 export default function Home() {
@@ -21,60 +23,78 @@ export default function Home() {
   const pokemonTwo = dataTwo.data?.pokemon;
 
   // TODO: replace any with actual type
-  const sendVote = (selected: any, notSelected: any) => {
+  const sendVote = (index: number, selected: any, notSelected: any) => {
     if (
-      selected?.stats[baseStatIndex]?.base_stat >
+      selected?.stats[baseStatIndex]?.base_stat >=
       notSelected?.stats[baseStatIndex]?.base_stat
     ) {
-      setIds([selected?.id, getRandomPokemon(notSelected?.id)]);
+      if (index === 0) {
+        setIds([selected?.id, getRandomPokemon(notSelected?.id)]);
+      } else {
+        setIds([getRandomPokemon(notSelected?.id), selected?.id]);
+      }
     } else {
-      console.log("wrong");
-      setIds(getOptionsForVote());
+      toast("Wrong", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      setTimeout(() => {
+        setIds(getOptionsForVote());
+      }, 100);
     }
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center mb-2">
-        Which Pokemon has higher
-        <span className="capitalize">
-          {pokemonOne?.stats[baseStatIndex]?.stat?.name}
-        </span>
-      </div>
-      <div className="border rounded p-8 flex justify-between max-w-2xl item-center">
-        <div className="w-64 h-64 bg-slate-300 flex flex-col items-center">
-          <button
-            className="w-full h-full"
-            onClick={() => sendVote(pokemonOne, pokemonTwo)}
-          >
-            {!dataOne.isLoading && (
-              <img
-                className="w-full"
-                src={pokemonOne?.sprites?.front_default ?? undefined}
-                alt="pokemon1"
-              />
-            )}
-          </button>
+    <>
+      <ToastContainer />
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-2xl text-center mb-2">
+          Which Pokemon has higher
+          <span className="capitalize">
+            {pokemonOne?.stats[baseStatIndex]?.stat?.name} ?
+          </span>
+        </div>
+        <div className="border rounded p-8 flex justify-between max-w-2xl item-center">
+          <div className="w-64 h-64 bg-slate-300 flex flex-col items-center">
+            <button
+              className="w-full h-full"
+              onClick={() => sendVote(0, pokemonOne, pokemonTwo)}
+            >
+              {!dataOne.isLoading && (
+                <img
+                  className="w-full"
+                  src={pokemonOne?.sprites?.front_default ?? undefined}
+                  alt="pokemon1"
+                />
+              )}
+            </button>
 
-          <div className="capitalize">{pokemonOne?.name}</div>
-        </div>
-        <div className="px-8 self-center">OR</div>
-        <div className="w-64 h-64 bg-slate-300 flex flex-col items-center">
-          <button
-            className="w-full h-full"
-            onClick={() => sendVote(pokemonTwo, pokemonOne)}
-          >
-            {!dataTwo.isLoading && (
-              <img
-                className="w-full"
-                src={pokemonTwo?.sprites?.front_default ?? undefined}
-                alt="pokemon1"
-              />
-            )}
-          </button>
-          <div className="capitalize">{pokemonTwo?.name}</div>
+            <div className="capitalize">{pokemonOne?.name}</div>
+          </div>
+          <div className="px-8 self-center">OR</div>
+          <div className="w-64 h-64 bg-slate-300 flex flex-col items-center">
+            <button
+              className="w-full h-full"
+              onClick={() => sendVote(1, pokemonTwo, pokemonOne)}
+            >
+              {!dataTwo.isLoading && (
+                <img
+                  className="w-full"
+                  src={pokemonTwo?.sprites?.front_default ?? undefined}
+                  alt="pokemon1"
+                />
+              )}
+            </button>
+            <div className="capitalize">{pokemonTwo?.name}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
